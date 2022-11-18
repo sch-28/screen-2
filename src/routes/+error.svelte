4a747d3576e7dca9f56e84c48dd755fa23fc8b2d
sch-28 <jansch2803@googlemail.com>
2022-11-18 22:07:03 +0100

    feat: added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { page } from '$app/stores';
+	export let status = 200;
+	export let message = 'ok';
+
+	$: {
+		if ($page.status != 200 && $page.error) {
+			status = $page.status;
+			message = $page.error?.message;
+		}
+	}
+</script>
+
+<div class="error">
+	<h1>{status}: {message}</h1>
+</div>
+
+<style>
+	.error {
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
