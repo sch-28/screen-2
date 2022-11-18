5dd448e9fd8e81312ef96df0aec629a727640dfe
sch-28 <jansch2803@googlemail.com>
2022-11-18 21:34:14 +0100

    chore: initialization

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Header from '../components/header.svelte';
+	import '../app.postcss';
+</script>
+
+<div class="bg-white dark:bg-gray-800 w-full h-full">
+	<Header />
+	<slot />
+</div>
---
+++ src/routes/record/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Alert, Button } from 'flowbite-svelte';
+</script>
+
+<Button>Default</Button>
+<Alert>
+	<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+</Alert>
